5f66486f52e0e209ea3f0c1a1b7911f6ade721a2
Tomi P <pitkanen.tomi@hotmail.com>
2021-08-19 08:19:23 +0300

    Add updated files

--- script.js
+++ script.js
@@ -1,61 +1,90 @@
-let playerScore = 0;
-let computerScore = 0;
+const btnRock = document.querySelector("#btnRock");
+const btnPaper = document.querySelector("#btnPaper");
+const btnScissors = document.querySelector("#btnScissors");
+const playerScore = document.querySelector("#player-score");
+const computerScore = document.querySelector("#computer-score");
+const tieScore = document.querySelector("#tie-score");
 
-function game() {
-    
-    const playerSelection = playerChoice();
-    const computerSelection = computerPlay();
-    
-    function computerPlay() {
-        const rps = ["rock", "paper", "scissors"];
-        const random = Math.floor(Math.random() * rps.length);
-        return(random, rps[random]);
-    }
+let playerWins = 0;
+let computerWins = 0;
+let tieWins = 0;
 
-    function playerChoice() {
-        return choice = window.prompt("Rock, paper or scissors?");
-    }
+btnRock.addEventListener("click", function(){
+    let playerSelection = "rock";
+    let computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+});
 
-    function playRound(playerSelection, computerSelection) {
-        if (playerSelection.toLowerCase() == computerSelection){
-            return "Its a tie!"
-        }
-        else if (playerSelection.toLowerCase() == "rock"){
-            if (computerSelection == "paper"){
-                computerScore++;
-                return "Computer won!"
-            }else{
-                playerScore++;
-                return "You won!"
-            }
-        }
-        else if (playerSelection.toLowerCase() == "scissors"){
-            if (computerSelection == "rock"){
-                computerScore++;
-                return "Computer won!"
-            }else{
-                playerScore++;
-                return "You won!"
-            }
-        }
-        else if (playerSelection.toLowerCase() == "paper"){
-            if (computerSelection == "scissors"){
-                computerScore++;
-                return "Computer won!"
-            }else{
-                playerScore++;
-                return "You won!"
-            }
-        }
-    }
+btnPaper.addEventListener("click", function(){
+    let playerSelection = "paper";
+    let computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+});
 
-console.log(playRound(playerSelection, computerSelection));
-console.log("Your score: " + playerScore);
-console.log("Computer score: "+ computerScore);
+btnScissors.addEventListener("click", function(){
+    let playerSelection = "scissors";
+    let computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+});
+
+function computerPlay() {
+    const rps = ["rock", "paper", "scissors"];
+    const random = Math.floor(Math.random() * rps.length);
+    return(random, rps[random]);
 }
 
-game();
-game();
-game();
-game();
-game();
+function playerWin() {
+    ++playerWins;
+    playerScore.textContent = "Your score: " + playerWins; 
+}
+
+function computerWin() {
+    ++computerWins;
+    computerScore.textContent = "Computer score: " + computerWins;
+}
+
+function tieWin() {
+    ++tieWins;
+    tieScore.textContent = "Ties: " + tieWins;
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() === computerSelection){
+        tieWin();
+    } else if (playerSelection.toLowerCase() === "rock"){
+        if (computerSelection === "paper"){
+            computerWin();
+        }else{
+            playerWin();
+        }
+    } else if (playerSelection.toLowerCase() === "scissors"){
+        if (computerSelection === "rock"){
+            computerWin();
+        }else{
+            playerWin();
+        }
+    } else if (playerSelection.toLowerCase() === "paper"){
+        if (computerSelection === "scissors"){
+            computerWin();
+        }else{
+            playerWin();
+        }
+    }
+    
+    if (computerWins === 5){
+        alert("You lost!");
+        reset();
+    } else if (playerWins === 5) {
+        alert("You won!");
+        reset();
+    }
+}
+
+function reset() {
+    playerWins = 0;
+    computerWins = 0;
+    tieWins = 0;
+    playerScore.textContent = "Your score: 0";
+    computerScore.textContent = "Computer score: 0";
+    tieScore.textContent = "Ties: 0";
+}
